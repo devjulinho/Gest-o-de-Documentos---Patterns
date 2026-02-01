175691f8f55726ea690ac66d4adc11717c2f702a
Luana Lima <luanalima.2006@hotmail.com>
2026-02-01 03:47:38 -0300

    Merge branch main into dev/luana

--- src/br/ifba/edu/inf011/model/GerenciadorDocumentoModel.java
+++ src/br/ifba/edu/inf011/model/GerenciadorDocumentoModel.java
@@ -22,6 +22,8 @@ public class GerenciadorDocumentoModel {
         this.factory = factory;
         this.gestor = new GestorDocumento();
         this.atual = null;
+
+        InicializadorChain.inicializar();        
     }
     
     //SINGLETON!!!!!!!!!!
